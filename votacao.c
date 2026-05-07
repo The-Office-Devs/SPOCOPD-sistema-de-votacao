73c2be11c35eeba2906e3dfa6259195cdd61151f
Fabricio Guerra <fabricio.guerra@itbeta2.com.br>
2026-05-07 19:45:26 -0300

    feat: Implementar consenso básico e validação de votos no sistema de votação

--- votacao.c
+++ votacao.c
@@ -6,8 +6,9 @@
 #define TOTAL_VOTOS 100000000
 #define CANDIDATOS 3
 
-//TODO: Ajustar tratamento do resto da divisão
-int main(int argc, char *argv[]) {
+// TODO: Ajustar tratamento do resto da divisão
+int main(int argc, char *argv[])
+{
 	int rank, size;
 
 	MPI_Init(&argc, &argv);
@@ -15,22 +16,28 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 
 	int votos_por_processo = TOTAL_VOTOS / size;
-	int *votos_locais = malloc(votos_por_processo * sizeof(int));
+	int *votos_locais =malloc(votos_por_processo * sizeof(int));
 	int local[CANDIDATOS] = {0};
 	int global[CANDIDATOS] = {0};
 	double inicio = MPI_Wtime();
 
 	srand(time(NULL) + rank);
 
-	for (int i = 0; i < votos_por_processo; i++) {
+	// Geração local dos votos
+	for (int i = 0; i < votos_por_processo; i++)
+	{
 		votos_locais[i] = rand() % CANDIDATOS;
 	}
 
-	for (int i = 0; i < votos_por_processo; i++) {
+	// Contagem local
+	for (int i = 0; i < votos_por_processo; i++)
+	{
 		int voto = votos_locais[i];
+
 		local[voto]++;
 	}
 
+	// Reduce principal da eleição
 	MPI_Reduce(
 		local,
 		global,
@@ -40,18 +47,56 @@ int main(int argc, char *argv[]) {
 		0,
 		MPI_COMM_WORLD);
 
+	// =========================
+	// CONSENSO BÁSICO
+	// =========================
+
+	// Soma local de votos
+	int total_local = 0;
+
+	for (int i = 0; i < CANDIDATOS; i++)
+	{
+		total_local += local[i];
+	}
+
+	// Soma global validada
+	int total_global = 0;
+
+	MPI_Reduce(
+		&total_local,
+		&total_global,
+		1,
+		MPI_INT,
+		MPI_SUM,
+		0,
+		MPI_COMM_WORLD);
+
 	double fim = MPI_Wtime();
 
-	if (rank == 0) {
-		printf("\nResultado Final\n");
+	if (rank == 0)
+	{
+		printf("\n=== Resultado Final ===\n");
 
 		int soma = 0;
-		for (int i = 0; i < CANDIDATOS; i++) {
-			printf("Candidato %d: %d votos\n", i, global[i]);
+		for (int i = 0; i < CANDIDATOS; i++)
+		{
+			printf("Candidato %d: %d votos\n",i,global[i]);
 			soma += global[i];
 		}
 
 		printf("\nTotal: %d votos\n", soma);
+
+		// Validação distribuída
+		if (total_global == TOTAL_VOTOS)
+		{
+			printf("Consenso validado: todos os votos foram contabilizados.\n");
+		}
+		else
+		{
+			printf("Erro de consenso: inconsistência detectada.\n");
+
+			printf("Esperado: %d | Recebido: %d\n",TOTAL_VOTOS,total_global);
+		}
 		printf("Tempo: %.4f segundos\n", fim - inicio);
 	}
 
